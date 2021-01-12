6e56cf2e190aa52f8f6fc1d656274e52c4f6cfd4
Ugurcan Cakal <ugurcan.cakal@gmail.com>
2021-01-12 17:31:30 +0300

    Haar-like feature detector and integral image convolution added

--- eye_track.py
+++ eye_track.py
@@ -3,51 +3,527 @@ ADM Tronics Image Processing Assignment
 
 Author : Ugurcan Cakal
 
-Effort
-2101091813 2 hours
+Effort Log
+2101091813 2 hours problem exploration
 
 '''
 
+import sys
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("eye_recording.flv")
+from cv2 import VideoWriter, VideoWriter_fourcc
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+# UTILITY CLASSES AND FUNCTIONS  
 
-    roi = frame[269:795, 537:1416]
-    rows, cols, _ = roi.shape
-    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+class HaarSurroundFeature:
+    '''
+    Haar like feature detection kernel 
+    Store the inner and outer values in the sparse form.
+    Return the dense form if required.
+    '''
 
-    gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+    def __init__(self, r_inner, r_outer=None, val=None):
+        '''
+         _________________
+        |        -ve      |
+        |     _______     |
+        |    |   +ve |    |
+        |    |   .   |    |
+        |    |_______|    |
+        |         <r1>    |
+        |_________<--r2-->|
 
-    _, threshold = cv2.threshold(gray_roi, 5, 255, cv2.THRESH_BINARY_INV)
-    edges = cv2.Canny(threshold,0,255)
+        Frobenius normalized values
+        
+        Want norm = 1 where norm = sqrt(sum(pixelvals^2)), so:
+        sqrt(count_inner*val_inner^2 + count_outer*val_outer^2) = 1
+        
+        Also want sum(pixelvals) = 0, so:
+        count_inner*val_inner + count_outer*val_outer = 0
+        
+        Solving both of these gives:
+        val_inner = sqrt(count_outer/(count_inner*count_outer + sq(count_inner)) );
+        val_outer = sqrt(count_inner/(count_inner*count_outer + sq(count_outer)) );
 
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+        Square radius normalised values
+        
+        Want the response to be scale-invariant, so scale it by the number of pixels inside it:
+        val_inner = 1/count = 1/r_outer^2
+        
+        Also want sum(pixelvals) = 0, so:
+        count_inner*val_inner + count_outer*val_outer = 0
+
+        Arguments:
+
+            r_inner(int):
+                inner radius of the kernel, r1.
+
+            r_outer(int):
+                outer radius of the kernel, r2.
+                Default:None 
+                In the default case, r2 = 3*r
+
+            val(tuple of float):
+                inner and outer values to be assigned
+                Default:None
+                In the default case, values are calculated using
+                Frobenius Normalization process.
+        '''
+
+        if r_outer is None:
+            r_outer = r_inner*3
+
+        count_inner = r_inner*r_inner;
+        count_outer = r_outer*r_outer - r_inner*r_inner
+
+        if val is None:
+            val_inner = 1.0 / (r_inner*r_inner)
+            val_outer = -val_inner*count_inner/count_outer
+
+        else:
+            val_inner = val[0];
+            val_outer = val[1];
+
+        self.val_in = val_inner
+        self.val_out= val_outer
+        self.r_in = r_inner
+        self.r_out = r_outer
+    
+    def get_kernel(self):
+        '''
+        Get the kernel in the dense matrix form
+        Mostly for debugging
+        '''
+        kernel = np.zeros(shape=(2*self.r_out, 2*self.r_out))
+        kernel.fill(self.val_out)
+
+        start = (self.r_out-self.r_in)
+        end = start + 2*self.r_in
+
+        kernel[start:end, start:end] = self.val_in
+        return kernel
+
+def to_gray(frame):
+    '''
+    Reduce the channels to 1(grayscale)
+
+        Arguments:
+
+            frame(np.ndarray of dim = 2 or 3):
+                the frame to be converted to grayscale
+
+        Return:
+
+            frame(np.ndarray of dim = 2):
+                grayscale frame
+    '''
+    if len(frame.shape)==2:
+        return frame
+    if (len(frame.shape)==3) and frame.shape[2] == 3:
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    elif (len(frame.shape)==3) and frame.shape[2] == 4:
+        return cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
+    else:
+        raise ValueError('Unsupported number of channels')
+
+def clip(frame, y, x):
+    '''
+    Clip the full frame to given x and y coordinates 
+    to get a smaller region of interest
+
+        Arguments:
+
+            frame(np.ndarray of dim = 2 or 3):
+                the frame to be clipped
+
+            y(tuple of int):
+                (ymin, ymax): y coordinates of 
+                region of interestin the y axis
+
+            x(tuple of int):
+                (xmin, xmax) x coordinates of 
+                region of interestin the y axis
+        
+        Returns:
+            frame(np.ndarray of dim = 2):
+                clipped frame
+
+    '''
+    return frame[y[0]:y[1], x[0]:x[1]]
+
+def save_video(frame_seq,filepath='test.avi',fps=24):
+    '''
+    Save the given frame sequence in the form of a video
+
+        Arguments:
+
+            px_frame_shape(int,int,int):
+                IMPORTANT : (height,width,channel)
+                frame shape in pixels. no control, choose carefully. 
+                default : 128,128,3
+    '''
+
+    fourcc = VideoWriter_fourcc(*'MP42')
+
+    frame_shape = frame_seq[0].shape
+
+    video = VideoWriter(filepath, fourcc, float(fps), (frame_shape[1], frame_shape[0]))  
+
+    for frame in frame_seq:
+        video.write(frame)
+    video.release()
+
+# def main(filepath):
+#     '''
+#     '''
+#     cap = cv2.VideoCapture(filepath)
+
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         roi = frame[269:795, 537:1416]
+#         rows, cols, _ = roi.shape
+#         gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+#         gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+
+#         _, threshold = cv2.threshold(gray_roi, 5, 255, cv2.THRESH_BINARY_INV)
+#         edges = cv2.Canny(threshold,0,255)
+
+#         contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+#         contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
 
-    if len(contours):
-        (x, y, w, h) = cv2.boundingRect(contours[0])
-        cv2.drawContours(roi, [contours[0]], -1, (255, 0, 0), 3) 
-        # cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        # cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
-        # cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
-        cv2.circle(roi, (x+int(w/2), y+int(h/2)), 1, (0, 255, 0), 2)
-        # cv2.circle(image, center_coordinates, radius, color, thickness)
+#         if len(contours):
+#             (x, y, w, h) = cv2.boundingRect(contours[0])
+#             cv2.drawContours(roi, [contours[0]], -1, (255, 0, 0), 3) 
+#             # cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#             # cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
+#             # cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
+#             cv2.circle(roi, (x+int(w/2), y+int(h/2)), 1, (0, 255, 0), 2)
+#             # cv2.circle(image, center_coordinates, radius, color, thickness)
 
 
-    cv2.imshow("ROI", gray_roi)
-    cv2.imshow("Threshold", threshold)
-    cv2.imshow("Canny", edges)
-    cv2.imshow("Highlighted", roi)
+#         cv2.imshow("ROI", gray_roi)
+#         cv2.imshow("Threshold", threshold)
+#         cv2.imshow("Canny", edges)
+#         cv2.imshow("Highlighted", roi)
 
-    key = cv2.waitKey(30)
-    if key == 27:
-        break
+#         key = cv2.waitKey(30)
+#         if key == 27:
+#             break
 
-cv2.destroyAllWindows()
+#     cv2.destroyAllWindows()
+
+def main(filepath, params):
+    '''
+    Main operation
+    '''
+    cap = cv2.VideoCapture(filepath)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # STEP 1 : Clip the frames such that they only include 1 eye 
+        # roi = clip(frame, (269,795), (537,1416))
+
+        # STEP 2 : Convert the frames to the grayscale version
+        # gray_roi = to_gray(roi)
+
+        gray_roi = to_gray(frame)
+
+        # STEP 3 : Approximate the pupil region to reduce the search space
+        region = initial_region_estimation(gray_roi,radius=32, step=16, h_factor=2, trim=False, outer=False)
+
+        # cv2.imshow("ROI", to_gray(roi))
+        cv2.imshow("REGION", region)
+
+        key = cv2.waitKey(30)
+        if key == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+def initial_region_estimation(frame, radius, step, h_factor=1.5, trim=True, outer=False):
+    '''
+    !!! ASSUMPTION : Pupil and iris together can be roughly described as
+    "a dark blob surrounded by a light background"
+
+    To find the pupil, use Haar-like feature detector on the integral image.
+    Integral image reduces the time complexity from O(N^2) to O(N)
+
+    The Haar-like feature is square, and so is only an approximation 
+    to the elliptical pupil shape.
+
+        Arguments:
+            frame(np.ndarray of dim = 2 or 3):
+                the frame to processed
+
+            radius(int):
+                inner radius for the HaarSurroundFeature kernel
+
+            step(tuple of int):
+                ystep, xstep
+                stepsize for kernel walk
+
+            h_factor(float):
+                factor of increase in calculating 
+                haar_radius = h_factor*radius
+                Default=1.5
+
+            trim(bool):
+                flag for trim or highlight
+                Default=True
+
+            outer(bool):
+                draw the outer rectangle or not
+                Used when trim option is false
+                Default=False
+    '''
+
+    # Make step a tuple in case a single number is passed
+    if not isinstance(step, (tuple, list)):
+        if isinstance(step, int):
+            step = (step, step)
+        else:
+            info = 'Unsupported step respresentation\n'
+            info += 'Example : step = (2,2)'
+            raise ValueError(info)
+
+    # frame = to_gray(frame)
+    row, col = to_gray(frame).shape
+    pad  = 2*radius
+    haar_radius = int(h_factor*radius)
+    
+    # STEP 1 : Get the integral image
+    # Need to pad by an additional 1 to get bottom & right edges.
+    frame_pad = cv2.copyMakeBorder(to_gray(frame), pad, pad, pad, pad, cv2.BORDER_REPLICATE) 
+    frame_int = cv2.integral(frame_pad)
+
+    # STEP 2 : Convolution
+    surround = HaarSurroundFeature(radius)
+    filtered, _, center = conv_int(frame_int, surround, step, pad)
+    region = get_roi(frame, center, haar_radius, trim, outer)
+
+    return region
+
+def conv_int(frame_int, kernel, step, padding):
+    '''
+    Convolution on the integral image.
+    Requires integral frame
+
+        -----------------------
+        Find best haar response
+        -----------------------
+
+                    _____________________
+                   |         Haar kernel |
+                   |                     |
+         __________|______________       |
+        | Image    |      |       |      |
+        |    ______|______|___.-r-|--2r--|
+        |   |      |      |___|___|      |
+        |   |      |          |   |      |
+        |   |      |          |   |      |
+        |   |      |__________|___|______|
+        |   |    Search       |   |
+        |   |    region       |   |
+        |   |                 |   |
+        |   |_________________|   |
+        |                         |
+        |_________________________|
+
+        Arguments:
+            frame_int(np.ndarray of dim=2):
+                integral frame
+
+            kernel(HaarSurroundFeature object):
+                surround kernel to be used in the convolution
+
+            step(tuple of int):
+                ystep, xstep
+                stepsize for kernel walk
+
+            padding(int):
+                number of padded pixels 
+
+        Returns:
+
+            frame_conv(np.ndarray of dim=2):
+                a frame consisting of kernel responses
+            
+            response(int):
+                the strongest kernel response
+
+            center(tuple of int): 
+                normalized pixel position of the strongest response
+
+    '''
+    # Init
+    row, col = frame_int.shape
+    row -= 1
+    col -= 1
+    y_min = 0 
+    x_min = 0
+    min_response = 255
+    y_step, x_step = step
+    f_shape = row - 2*padding, col- 2*padding
+    
+    frame_conv = np.zeros(shape = f_shape, dtype=np.uint8)    
+
+    # Convolution on the integral frame
+    for y in range(padding, row-padding, y_step):
+        for x in range(padding, col-padding, x_step):
+            response = isum_response(frame_int, kernel, padding, (y, x))
+            # Keep track of minimum response
+            if response<min_response:
+                min_response = response
+                y_min = y - padding
+                x_min = x - padding
+            frame_conv[y-padding,x-padding] = response
+
+    center = (x_min,y_min)
+
+    return frame_conv, response, center
+
+def get_roi(frame, center, radius, trim=False, outer=False, color=(0, 255, 0), thickness=2):
+    '''
+    Region of Interest given the frame defined by 
+    radius and the center
+    trim or highlight
+
+    Arguments:
+        frame(np.ndarray of dim = 2 or 3):
+            the frame to processed
+
+        center(tuple of int):
+            x,y coordinate of the center of region of interest
+
+        radius(int):
+            radius of the region of interest, 
+
+        trim(bool):
+            flag for trim or highlight
+            Default=False(will highlight)
+
+        outer(bool):
+            draw the outer rectangle or not
+            Used when trim option is false
+            Default=False
+
+        color(tuple of int)
+            BGR color of the rectangle highlight
+            Used when trim option is false
+            Default=(0, 255, 0) (green)
+
+        thickness(int):
+            Line thickness of the rectangle highlight
+            Used when trim option is false
+            Default=2
+
+    Returns:
+        frame(np.ndarray of dim = 2 or 3):
+            highlighted or clipped frame
+    '''
+    start = tuple([val-radius for val in center])
+    end = tuple([val+radius for val in center])
+
+    
+
+    if trim:
+        y = (start[1], end[1])
+        x = (start[0], end[0])
+        frame = clip(frame, y, x)
+    else:
+        cv2.rectangle(frame, start, end, color, thickness)
+        if outer:
+            start_big = tuple([val-3*radius for val in center])
+            end_big = tuple([val+3*radius for val in center])
+            cv2.rectangle(frame, start_big, end_big, color, thickness)
+
+    return frame
+
+def isum_response(frame_int, kernel, padding, pos):
+    '''
+    Kernel response in the respected position calculated on the integral image
+    p00+p11-p01-p10 gives the area sum on the ingtegral image. 
+    p11 -> inside the desired area
+    p00, p10, p01 -> neighbours
+
+        ¦         ¦ 
+        |         |  p00._____________________.p01
+        |         |     |         Haar kernel |
+        |         |     |                     |
+        |         |     |   p00._______.p01   |
+        |-padding-|     |      |       |      |
+        |         |     |      | (x,y) |      |
+        |         |     |      |_______|      |
+        |         |     |   p10'       'p11   |
+        |         |     |                     |
+        |         |     |_____________________|
+        |         |  p10'                     'p11
+        ¦         ¦
+
+        Arguments:
+
+            frame_int(np.ndarray of dim=2):
+                integral frame
+
+            kernel(HaarSurroundFeature object):
+                surround kernel to be used in the convolution
+
+            padding(int):
+                number of padded pixels
+
+            pos(tuple of int):
+                represent y,x position
+
+        Returns:
+            response(int):
+                Kernel response of the respective pixel
+
+    '''
+    # Init
+    ylim, xlim = frame_int.shape
+    ylim -= 1
+    xlim -= 1
+    y, x = pos
+    r_in = kernel.r_in
+    r_out = kernel.r_out
+    
+    # Inner area sum
+    inner_sum = frame_int[y-r_in,x-r_in] + frame_int[y+r_in, x+r_in] - frame_int[y-r_in, x+r_in] - frame_int[y+r_in, x-r_in]
+    
+    # Outer area tends not to fit the padded image. 
+    # Make sure that no indexing error will be raised
+    p00 = y-r_out if y-r_out>0 else 0, x-r_out if x-r_out>0 else 0
+    p11 = y+r_out if y+r_out<ylim else ylim, x+r_out if x+r_out<xlim else xlim
+    p01 = y-r_out if y-r_out>0 else 0, x+r_out if x+r_out<xlim else xlim
+    p10 = y+r_out if y+r_out<ylim else ylim, x-r_out if x-r_out>0 else 0
+    outer_sum = frame_int[p00] + frame_int[p11] - frame_int[p01] - frame_int[p10] - inner_sum
+
+    # Calculate the response (O(N) :))
+    response = kernel.val_in*inner_sum + kernel.val_out*outer_sum
+    
+    return response
+
+def pupil_segmentation():
+    '''
+    
+    '''
+    None
+
+if __name__ == '__main__':
+
+    params = {'radius_min' : 3,
+              'radius_max' : 8}
+
+    if len(sys.argv)>1:
+        main(sys.argv[1], params)
+    
+
